c1f5be00cb9b87f5778ffdb0592b7d42f5de26e4
crizostomo <diogo_crizostomo@hotmail.com>
2023-01-24 17:17:27 -0300

    first commit

---
+++ src/test/java/com/beverage/beverageapiauth/BeverageApiAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beverage.beverageapiauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeverageApiAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
