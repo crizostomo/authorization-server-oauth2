02f0244b5dbc6ddc4b50ac347e4c599792b93de6
crizostomo <diogo_crizostomo@hotmail.com>
2023-01-24 21:03:23 -0300

    22.9 | Configuring the Authorization Server Token with Password Credtials and Opaque Tokens

--- src/main/java/com/beverage/beverageapiauth/AuthorizationServerConfig.java
+++ src/main/java/com/beverage/beverageapiauth/AuthorizationServerConfig.java
@@ -1,10 +1,36 @@
 package com.beverage.beverageapiauth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("beverage-web")
+                .secret(passwordEncoder.encode("web123"))
+                .authorizedGrantTypes("password") // Type of flow used = password
+                .scopes("write", "read")
+                .accessTokenValiditySeconds(60 * 60 * 6); // The standard time is 12 hours
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager); // It needs this, otherwise the 'password flow' does not work
+    }
 }
